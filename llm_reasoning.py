46eba8c95dac4da8498e3ff3fb2cd81e3b2ce89c
madhih2000 <106566123+madhihtvs@users.noreply.github.com>
2025-03-16 11:29:51 +0800

    Removing API key

--- llm_reasoning.py
+++ llm_reasoning.py
@@ -1,8 +1,9 @@
 from groq import Groq
 import streamlit as st
 import pandas as pd
+import requests
 
-API_KEY = "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"
+groq_api_key = st.secrets["groq"]["API_KEY"]
 
 def explain_box_plot_with_groq(df, material_column="Material Number"):
     """
